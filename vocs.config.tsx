5b3fc94a3cc8d2362201cff6079134a48ce07f0b
Oleg <olegkron.cc@gmail.com>
2024-10-07 10:48:22 +0200

    init

---
+++ vocs.config.tsx
@@ -0,0 +1,111 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+	title: 'Concero Docs',
+	titleTemplate: '%s – Concero Docs',
+	editLink: {
+    pattern: 'https://github.com/concero/docs/edit/master/docs/pages/:path',
+    text: 'Suggest changes to this page',
+	},
+	// head() {
+ //    return (
+ //      <>
+ //        <script src="https://cdn.usefathom.com/script.js" data-site="IBTUTKMT" defer />
+ //      </>
+ //    )
+ //  },
+	// banner: 'Head to our new [Discord](https://discord.gg/lanca)!',
+	description: 'Build the next big thing on Lanca.',
+	baseUrl: 'https://docs.concero.io',
+	iconUrl: 'https://concero.io/favicon.ico',
+	logoUrl: {
+		light: '/logo-light.svg',
+		dark: '/logo-dark.svg',
+	},
+	// theme: {
+	// 	// accentColor: '#5E43FF',
+	// },
+	rootDir:	'docs',
+	ogImageUrl: {
+    '/': '/og.png',
+    '/docs': 'https://docs.lanca.io/og?logo=%logo&title=%title&description=%description',
+  },
+
+	// font: {
+	//   google: 'Manrope'
+	// },
+	topNav: [
+		{
+			text: 'Get in touch',
+			link: 'mailto:support@lanca.io',
+		},
+		// {
+		// 	text: 'Whitepaper',
+		// 	link: 'https://www.concero.io/whitepaper.pdf',
+		// },
+	],
+	socials: [
+		{
+			icon: 'discord',
+			link: 'https://discord.gg/lanca',
+		},
+		// {
+		// 	icon: 'github',
+		// 	link: 'https://github.com/lanca',
+		// },
+		{
+			icon: 'x',
+			link: 'https://twitter.com/lanca_io',
+		},
+
+	],
+	sidebar: [
+		{
+			text: 'What is Lanca?',
+			link: '/what-is-lanca',
+		},
+		{
+			text: 'Getting Started',
+			link: '/getting-started',
+		},
+
+		{
+			text: 'JS SDK (coming soon)',
+			link: '/js-sdk',
+			items: [
+				{
+					text: 'Overview',
+					link: '/js-sdk/overview',
+				},
+				{
+					text: 'Installation',
+					link: '/js-sdk/installation',
+				},
+				{
+					text: 'Usage',
+					link: '/js-sdk/usage',
+				},
+
+			]
+		},
+		{
+			text: 'API Endpoints (coming soon)',
+			// link: '/api-endpoints',
+			// items: [
+			// 	{
+			// 		text: 'Overview',
+			// 		link: '/api/overview',
+			// 	},
+			// 	{
+			// 		text: 'Routing',
+			// 		link: '/api/routing',
+			// 	},
+			// 	{
+			// 		text: 'Tracking',
+			// 		link: '/api/tracking',
+			// 	},
+			//
+			// ]
+		}
+	],
+})
